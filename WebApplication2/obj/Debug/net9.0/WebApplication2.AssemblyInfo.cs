9615cca97e4d11e3d280a2f9459ad5b989fffef2
Niko <263522@student.pwr.edu.pl>
2025-06-25 15:52:06 +0200

    nic sie nie zmienia

--- WebApplication2/obj/Debug/net9.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/obj/Debug/net9.0/WebApplication2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eae3b8248cb3b271c3c5ef4dfa6ed41fad185f42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3b26fdaf6637bf54caa1109b4f7bc324724d515")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
